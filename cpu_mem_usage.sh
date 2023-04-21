695d2fb5834a1996a01a80bd9e7977c592d4bcff
R-Mahe <rmahe390@gmail.com>
2023-04-21 12:20:23 +0530

    new shell script for cpu,mem,disk usage

---
+++ cpu_mem_usage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#set -x
+file=usage.txt
+cpu=`top -bn1 | grep "Cpu(s)" | awk -F' ' '{print $2}'`
+mem=`free -g | grep 'Mem:' | awk -F' ' '{print $2}'`
+diskused=`df -Ph | grep -e "/vagrant" | awk -F' ' '{print $3}'`
+diskav=`df -Ph | grep -e "/vagrant" | awk -F' ' '{print $4}'`
+cat << EOF > $file
+----------------------------------------
+|          |          |   Disk Usage   |
+|   CPU    |   MEM    |----------------|
+|          |          |  used  | free  |
+|----------|----------|--------|-------|
+|   $cpu    |    $mem     |  $diskused  | $diskav  |
+----------------------------------------
+EOF
